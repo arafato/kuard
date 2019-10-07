3bb9a9d14d3a520189c04b451f3d9568e8f0f67b
arafato <oliver.arafat@outlook.com>
2019-10-07 18:09:01 +0200

    updated

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -52,7 +52,7 @@ export default class App extends React.Component {
           </div>
           <Disconnected ref={el => this.disconnected = el}/>
           <h2><samp>{this.props.page.hostname}</samp></h2>
-          <div>Modified AGAIN (6)! Demo application version <i>{this.props.page.version}</i></div>
+          <div>Modified AGAIN (7)! Demo application version <i>{this.props.page.version}</i></div>
           <div>Serving on {addrs}</div>
         </div>
 
